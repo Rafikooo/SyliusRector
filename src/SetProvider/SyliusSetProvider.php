aafbd8911297ec798ec32441ee00d042dee59e54
Rafikooo <thecrazyrafik@gmail.com>
2025-11-25 22:36:41 +0100

    WIP

---
+++ src/SetProvider/SyliusSetProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\SyliusRector\SetProvider;
+
+use Rector\Set\Contract\SetProviderInterface;
+use Rector\Set\ValueObject\ComposerTriggeredSet;
+
+final class SyliusSetProvider implements SetProviderInterface
+{
+    /** @return ComposerTriggeredSet[] */
+    public function provide(): array
+    {
+        return [
+            new ComposerTriggeredSet(
+                'symfony',
+                'bitbag/elasticsearch-plugin',
+                '5.2',
+                __DIR__ . '/../../config/sets/sylius/b2b-kit/elasticsearch-plugin.php'
+            ),
+
+            new ComposerTriggeredSet(
+                'symfony',
+                'sylius/b2b-kit',
+                '3.0',
+                __DIR__ . '/../../config/sets/sylius/b2b-kit/b2b-kit-3.0-with-new-namespaces.php'
+            ),
+        ];
+    }
+}
